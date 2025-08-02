afadf6aeec106792083f8421048ad4d9f133cd9b
Melisha Shrestha <msthaa07@gmail.com>
2025-08-02 17:12:16 +1000

    add about page

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function About(){
+    return (
+        <main>
+            <h1>About</h1>
+            <p>Name: Melisha Shrestha</p>
+            <p>Student ID: 21923855</p>
+        </main>
+    );
+}
