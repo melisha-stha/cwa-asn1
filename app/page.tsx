905a565a0825d623845f9b121c08a7424419a9ce
Melisha Shrestha <msthaa07@gmail.com>
2025-08-26 22:01:33 +1000

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home(){
-  return <h2>Home Page</h2>;
+export default function Home() {
+  return (
+    <div className="container text-center mt-5">
+      <h2>Welcome to my CSE3CSA Assignment 1 Website</h2>
+      <p className="mt-4">
+        I am <strong>Melisha Shrestha</strong>, currently in my third year studying a Bachelor of Computer Science majoring 
+        in Software Engineering. I created this website as a part of my <strong>CSE3CWA Assignment 1</strong>. 
+        You can also find a video explaining how to use this website on the <em>About</em> page.
+      </p>
+    </div>
+  );
 }
